13ce438fbe54eb7d3aeaa12790479f7cb5664cdb
Sesaque Cruz <118492168+sesaquecruz@users.noreply.github.com>
2022-12-15 20:03:04 -0300

    Criação da estrutura do projeto

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# dio-spring-data-jpa
+## Spring Data JPA na prática - DIO
+
+Este projeto é uma solução para o desafio do `Lab Conhecendo Spring Data JPA na prática` proposto pela *Digital Innovation One*.
+
+> Desafio: Criar uma `API RESTful` utilizando `Spring Data JPA` para gerenciar o cadastro de alunos de uma academia.
---
+++ src/main/java/com/academia/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.academia.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Bem vindo à academia!");
+    }
+}
